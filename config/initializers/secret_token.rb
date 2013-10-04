20e3b172f403fba136ee0563b3ad4296c1b7a3ab
masjamez <james@cleere.net>
2013-10-04 16:29:19 +0700

    Start of the JumpStartRails build, as far as adding gemfiles for ActiveAdmin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jamesblog::Application.config.secret_key_base = '7096f34f43b48454b81508728c3daaee56275a62299ad526455eef6e319d1c11545914ec45fb1c8eaf0ad7312fa56a4d461902601394ee522d82b910ff7d8a5e'
